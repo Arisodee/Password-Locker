c4a6d2d393a42b2a929c04a27efcecb7b9de42ce
Arisodee <arisodee@gmail.com>
2020-11-22 22:28:34 +0300

    Add Set up method to run before each test cases and a test to check if the object is nitialized properly

--- password_test.py
+++ password_test.py
@@ -9,3 +9,22 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Arisodee","AbCd*.91") # create a contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"Arisodee")
+        self.assertEqual(self.new_user.password,"AbCd*.91")
+
+
+if __name__ == '__main__':
+    unittest.main()
